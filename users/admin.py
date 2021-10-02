1a0e7873689a9511f6c640b32abe09a60fb75295
iamnaf <nafsulaiman@gmail.com>
2021-10-02 09:23:20 +0100

    add urls.py to users

---
+++ users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    change = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'birthday', 'age']
+
+admin.site.register(CustomUser, CustomUserAdmin)
