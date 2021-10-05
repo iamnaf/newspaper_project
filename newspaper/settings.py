519f4dced70d8e13d08bdeea9af62a151d8b2c57
iamnaf <nafsulaiman@gmail.com>
2021-10-05 16:25:27 +0100

    add change password and crispy tags

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'pages',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
